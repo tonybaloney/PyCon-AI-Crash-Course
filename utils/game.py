b67747eb30e29e8221efe168d6c4f12e1ffb0193
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-05-08 15:34:13 +1000

    Add card game and rules

---
+++ utils/game.py
@@ -0,0 +1,44 @@
+RULES = """
+The game is called **Elemental Clash**
+
+Objective: The goal of the game is to collect the most points by winning rounds with your elemental cards.
+
+## Setup:
+
+- The game is played with a standard deck of cards.
+- There are four types of elemental cards: Fire, Water, Earth, and Air.
+- "Fire" is spades
+- "Water" is diamonds
+- "Earth" is hearts
+- "Air" is clubs
+- Card values are Ace-High (2 to 10, J, Q, K, A) so 2 is the lowest value
+- Shuffle the deck and deal 5 cards to each player. The remaining cards form the draw pile.
+
+## Gameplay:
+
+### Starting the Round:
+
+Each player selects one card from their hand and places it face down on the table.
+Once all players have chosen their cards, they reveal them simultaneously.
+
+### Determining the Winner:
+
+The players eliminate each other with the following rules:
+
+- Fire beats Air
+- Air beats Earth
+- Earth beats Water
+- Water beats Fire
+
+If there is a tie (two or more players play cards with the same element), the value the winner (e.g. 7 beats 3):
+
+### Drawing New Cards:
+
+After each round, players draw one card from the draw pile to replenish their hand back to 5 cards.
+If the draw pile is empty, players continue playing with the cards they have left.
+
+## Ending the Game:
+
+The game ends when all cards have been played.
+Players count the total number of cards they have collected.
+The player with the most cards wins the game."""
